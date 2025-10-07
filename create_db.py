b68ec44d8b3b4d76a879e993d013fb08d18be99a
FRicardo5 <felipe.ferreira@bsatech.io>
2025-10-07 14:30:33 -0300

    feat: criando banco de dados

--- create_db.py
+++ create_db.py
@@ -1,1 +1,24 @@
 import sqlite3
+ 
+conn = sqlite3.connect('escola.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS estudantes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTEGER
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS disciplinas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_disciplina TEXT,
+    estudante_id INTEGER,
+    FOREIGN KEY (estudante_id) REFERENCES estudantes(id)
+)
+''')
+
+conn.commit()
+conn.close()
